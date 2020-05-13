fb02c2c73e91d6d9c087ddd6602d13a2a1bcc9cf
Rolf Erik Lekang <me@rolflekang.com>
2020-05-13 08:41:09 +0200

    Add RSS feed (#7)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'BrainBase Blogg',
+    description: '',
+    siteUrl: 'https://blogg.brainbase.no',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -54,6 +56,59 @@ module.exports = {
         pedantic: true,
         gfm: true,
         plugins: [`gatsby-remark-emoji`, `gatsby-remark-reading-time`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/feed.xml',
+            title: 'BrainBase Blogg',
+          },
+        ],
       },
     },
   ],
