01754dcf3faac14e50845d52ee898c6939d85310
Daniel Jackson <daniel@snilldesign.no>
2022-10-15 10:50:18 +0200

    Change from Brainbase to BrainBase in hero text

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ import './hero.css'
 const Hero = ({ title }) => (
   <section className="blog-hero">
     <p className="typography__paragraph ">
-      <a className="typography__link" href="https://www.brainbase.no/">Brainbase</a> er et nettverk av over 800 konsulenter i teknologibransjen. 
+      <a className="typography__link" href="https://www.brainbase.no/">BrainBase</a> er et nettverk av over 800 konsulenter i teknologibransjen. 
     </p>
     <p className="typography__paragraph blog-hero__last-paragraph">
       Her kan du lese litt om våre <span style={{display: "inline-block"}}>erfaringer. 👇</span>
